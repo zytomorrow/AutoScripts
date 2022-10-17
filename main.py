18f6e8435491684f0a83296c63f6a730d9c2640b
zytomorrow <z794672847@gmail.com>
2022-10-18 00:10:57 +0800

    :tada:

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-from traceback import print_tb
-from sklearn import config_context
 import yaml
 """
 程序入口文件
@@ -20,15 +18,18 @@ if __name__ == '__main__':
         # 解析配置文件
         with open(CONFIG_FILE_PATH, encoding='utf8') as f:
             content = yaml.load(f.read(), Loader=yaml.SafeLoader)
-        print(content)
+
+        # todo: 通用配置
+
         # 执行脚本配置
         for script in content['scripts']:
-            script_name, script_config = script.keys()[0], script_config.valuse()
+            script_name, script_config = list(script.keys())[0], list(script.values())[0]
             if script_config['enable']:
                 pass
 
         # 推送信息配置
-        for push_service_name, push_service_config in content['pushnotice']:
+        for push_service in content['pushnotice']:
+            push_service_name, push_service_config = list(push_service.keys())[0], list(push_service.values())[0]
             if push_service_config['enable']:
                 # todo: 加载推送信息进队列
                 pass
--- scripts/icc2022.py
+++ scripts/icc2022.py
@@ -5,10 +5,30 @@
 PT站www.icc2022.com每日登录
 """
 
-class Client(object):
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-    
-    def init_login_page(self):
-        pass
+from lib.apiHandler import ApiHandle
+from pyquery import PyQuery as pq
+
+
+def run(username, passwd):
+    # 初始化session
+    api_handler = ApiHandle()
+    # 初始化网页
+    login_page_code = api_handler.get_url('https://www.icc2022.com/login.php').text
+    # 获取验证码图片地址
+    doc = pq(login_page_code)
+    print(doc('#nav_block > form:nth-child(4) > table > tbody > tr:nth-child(4) > td:nth-child(2) > img'))
+
+    # 获取
+
+#
+# class Client(object):
+#     def __init__(self, username, password):
+#         self.username = username
+#         self.password = password
+#         self.api_handle = ApiHandle()
+#
+#     def init_login_page(self):
+#         self.api_handle.ge
+
+if __name__ == '__main__':
+    run(111, 111)

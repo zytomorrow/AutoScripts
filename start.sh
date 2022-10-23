e16faf0fac2f912539c44a0f67b889ba64b96fe6
zytomorrow <z794672847@gmail.com>
2022-10-23 13:35:52 +0800

    :bug: 修改定时任务

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python /app/main.py
-echo -e "$CRONTIME $USER pyhton /app/main.py >> /var/log/cron.log 2>&1\n#empty line" >/etc/cron.d/mycron
+echo -e "$CRONTIME $USER pyhton /app/main.py 2>&1\n#empty line" >/etc/cron.d/mycron
 crontab /etc/cron.d/mycron
 cron && tail -f /app/AutoScripts.log
